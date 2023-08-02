f3efe0697caaa77a078b4e69403ba07ecca0c1bf
S.Paksima <s.paksima@emofid.com>
2023-08-02 13:01:26 +0330

    income in models and some beautification

--- web/models.py
+++ web/models.py
@@ -7,4 +7,13 @@ class Expense(models.Model):
     amount = models.BigIntegerField()
     date = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return "{}-{}".format(self.date, self.amount)
 
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return "{}-{}".format(self.date, self.amount)
